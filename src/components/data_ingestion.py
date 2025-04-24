78e412fc383dfdea9016ff627766dadc60579308
Gautham Vijayan <gauthamnvj@gmail.com>
2025-04-24 13:19:10 +0530

    Stopped tracking artifacts and catboost_info, updated notebooks and ingestion code

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path :str = os.path.join('Artifacts','train.csv')
+    test_data_path :str = os.path.join('Artifacts','test.csv')
+    raw_data_path :str = os.path.join('Artifacts','data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info('train_test_split initited')
+            
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=33)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )      
+        except  Exception as e:
+            raise CustomException(e,sys)
+            
+
+
+
+if __name__ == '__main__':
+    print("✅ Script has started")  # Add this line first
+    obj = DataIngestion()
+    train_path, test_path = obj.initiate_data_ingestion()  # Make sure function name is correct
+    print(f"✅ Train Path: {train_path}")  # Confirm train path is returned
+    print(f"✅ Test Path: {test_path}")    # Confirm test path is returned
+
